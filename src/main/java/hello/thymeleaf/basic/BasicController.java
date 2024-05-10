e5c3602ea14ed0c21aced78e177a6992cbcc269c
김유진 <gracecancho@gmail.com>
2024-05-10 11:58:07 +0900

    변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -14,6 +20,7 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-basic";
     }
+
     /**
      * Unescape
      */
@@ -22,4 +29,38 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-unescaped";
     }
+
+    // 변수 - SpringEL
+    @GetMapping("/variable")
+    public String variable(Model model) {
+
+        User userA = new User("김유진", 35);
+        User userB = new User("변새롬", 34);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+    // 변수 - SpringEL
 }
